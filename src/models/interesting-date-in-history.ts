01fe9e04eef2e681049d59e3bf3a17809ab9ea9a
Rob <rob_452@hotmail.co.uk>
2023-01-16 18:36:36 +0000

    Add models for word of the day and dates in history

---
+++ src/models/interesting-date-in-history.ts
@@ -0,0 +1,5 @@
+export type InterestingDateInHistory = {
+  date: string;
+  event: string;
+  school: string;
+};
---
+++ src/models/word-of-the-day.ts
@@ -0,0 +1,6 @@
+export type WordOfTheDay = {
+  date: string;
+  word: string;
+  definition: string;
+  context: string;
+};

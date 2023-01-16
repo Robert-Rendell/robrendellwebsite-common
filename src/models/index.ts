d8041513cf47910b74fe0fb349dff1b82f0eec6b
Rob <rob_452@hotmail.co.uk>
2023-01-16 18:41:59 +0000

    Forgot add new models to index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,5 @@ export * from "./sudoku";
 export * from "./google-keep";
 export * from "./ip-location";
 export * from "./page-viewer-document";
+export * from "./interesting-date-in-history";
+export * from "./word-of-the-day";

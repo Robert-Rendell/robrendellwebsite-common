d41737b26464b6f605b58ee05edb719c4f872696
Rob <rjprendell@gmail.com>
2023-03-03 22:32:01 +0000

    Battleships - fix BattleshipsMove to reflect what's being submitted

--- src/models/battleships/battleships-move.ts
+++ src/models/battleships/battleships-move.ts
@@ -1,9 +1,4 @@
 export type BattleshipsMove = {
-  coords: BattleshipsMoveCoords;
+  coords: [number, number];
   datetime: string;
-};
-
-export type BattleshipsMoveCoords = {
-  x: number;
-  y: number;
 };

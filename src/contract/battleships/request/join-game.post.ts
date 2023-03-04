ea9a9ba764c5554524c70bea9e82e47af1a9184e
Rob <rjprendell@gmail.com>
2023-03-04 11:29:32 +0000

    Battleships - add request for join game POST

---
+++ src/contract/battleships/request/join-game.post.ts
@@ -0,0 +1,3 @@
+import { BattleshipsGame, BattleshipsUser } from "../../../models";
+
+export type PostBattleshipsJoinGameRequest =Pick<BattleshipsGame, "gameId"> & Pick<BattleshipsUser, "username">;

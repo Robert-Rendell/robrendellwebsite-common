473b9206a904cbdd7237a6656bdf48ffcd15b8a9
Rob <rjprendell@gmail.com>
2023-03-09 14:32:50 +0000

    Battleships: Add typing for player board cell values

--- src/models/battleships/battleships-board.ts
+++ src/models/battleships/battleships-board.ts
@@ -1,1 +1,10 @@
-export type BattleshipsBoard = number[][];
+export type BattleshipsBoard = BattleshipBoardType[][];
+
+export type BattleshipBoardStateTypes = "Hit" | "Miss" | "Unknown";
+export type BattleshipBoardType = 1 | 0 | -1;
+export const BattleshipCellStatus: Record<BattleshipBoardStateTypes, BattleshipBoardType> = {
+    Hit: 1,
+    Miss: 0,
+    Unknown: -1,
+  }
+

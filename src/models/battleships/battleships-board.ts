5fb6639ba10338136f3230d5e484d45b39a23d0b
Rob <rjprendell@gmail.com>
2023-03-09 15:25:04 +0000

    Battleships: workaround for cjs loader issue

--- src/models/battleships/battleships-board.ts
+++ src/models/battleships/battleships-board.ts
@@ -2,7 +2,11 @@ export type BattleshipsBoard = BattleshipsBoardType[][];
 
 export type BattleshipsBoardStateTypes = "Hit" | "Miss" | "Unknown";
 export type BattleshipsBoardType = 1 | 0 | -1;
-export const BattleshipsBoardState = {
+export const BattleshipsBoardState: {
+  Hit: BattleshipsBoardType;
+  Miss: BattleshipsBoardType;
+  Unknown: BattleshipsBoardType;
+} = {
   Hit: 1,
   Miss: 0,
   Unknown: -1,

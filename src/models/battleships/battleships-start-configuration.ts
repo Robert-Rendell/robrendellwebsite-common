ca19bf07083129db1db8126096d8e9873b3b128a
Rob <rjprendell@gmail.com>
2023-03-10 14:46:31 +0000

    Battleships: Configuration needs different board to BattleshipsGame (hits/misses)

--- src/models/battleships/battleships-start-configuration.ts
+++ src/models/battleships/battleships-start-configuration.ts
@@ -1,8 +1,10 @@
-import { BattleshipsBoard } from "./battleships-board";
+import { BattleshipType } from "./battleship-type";
 import { BattleshipsGame } from "./battleships-game";
 import { BattleshipsUser } from "./battleships-user";
 
+export type BattleshipsConfigurationBoard = BattleshipType[][];
+
 export type BattleshipsStartConfiguration = {
-  configuration: BattleshipsBoard;
+  configuration: BattleshipsConfigurationBoard;
 } & Pick<BattleshipsGame, "gameId"> &
   Pick<BattleshipsUser, "username">;

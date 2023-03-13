3f7881ee4b84d7a51581fa9cd6ff1c65db389757
Rob <rjprendell@gmail.com>
2023-03-13 21:40:06 +0000

    Battleships: Change shipsRemaining to playerShips with more descriptive model +1

--- src/models/battleships/battleships-game.ts
+++ src/models/battleships/battleships-game.ts
@@ -2,7 +2,7 @@ import { BattleshipsBoard } from "./battleships-board";
 import { BattleshipsMove } from "./battleships-move";
 import { BattleshipsUsername } from "./battleships-user";
 import { BattleshipsGameState } from "./battleships-game-state";
-import { BattleshipStatuses, BattleshipType } from "./battleship-type";
+import { BattleshipStatuses } from "./battleship-type";
 
 export type Player1 = 0;
 export type Player2 = 1;
